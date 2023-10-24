79891da8ddcbbb7d448e8c03167e7d65407e9b76
kfggww <kfggww@outlook.com>
2023-10-24 08:33:05 +0800

    [Test]: Add new test for phead.
    Using a pre-allocated array to drive phead.

--- src/test.cpp
+++ src/test.cpp
@@ -5,6 +5,8 @@
 #include "drivers/owl_motor.h"
 #include "drivers/owl_phead.h"
 
+static uint8_t buffer[120][48];
+
 void test_init() {
   Serial.begin(115200);
   adc_init();
@@ -32,14 +34,48 @@ void test_motor() {
   }
 }
 
-void test_phead() {
+/**
+ * 打印6大行数据, 每行宽度20, 每个小行打印前需要重置data.
+ */
+void test_phead_with_reset() {
   uint8_t data[48];
   for (int i = 0; i < 6; i++) {
     for (int j = 0; j < 20; j++) {
       memset(data, 0, 48);
-      memset(data + 8 * i, 0x55, 48 - 8 * i);
+      memset(data + 8 * i, 0xf0, 48 - 8 * i);
       phead_draw_line(data, 48);
     }
+  }
+}
+
+/**
+ * 初始化一次data之后, 循环打印20行.
+ *
+ * @bug 1) 通过spi发送到打印头之后, 原始数据会被修改;
+ *      2) 使用这种方式调用, 后续的打印也会失败;
+ */
+void test_phead_without_reset() {
+  uint8_t data[48];
+  memset(data, 0xf0, 48);
+  for (int j = 0; j < 20; j++) {
+    phead_draw_line(data, 48);
+  }
+}
+
+/**
+ * 使用预先分配的buffer作为数据源, 驱动打印头.
+ */
+void test_phead_with_buffer() {
+  // 初始化buffer
+  for (int i = 0; i < 6; i++) {
+    for (int j = 0; j < 20; j++) {
+      memset(buffer[i * 20 + j] + 8 * i, 0xf0, 48 - 8 * i);
+    }
+  }
+
+  // 开始打印
+  for (int k = 0; k < 120; k++) {
+    phead_draw_line(buffer[k], 48);
   }
 }
 
@@ -48,8 +84,10 @@ void test_run() {
   Serial.println("test_run begin...");
 
   test_adc_and_gpio();
-  test_motor();
-  test_phead();
+  // test_motor();
+  test_phead_with_reset();
+  // test_phead_without_reset();
+  test_phead_with_buffer();
 
   Serial.println("test_run end...");
   for (;;) {
